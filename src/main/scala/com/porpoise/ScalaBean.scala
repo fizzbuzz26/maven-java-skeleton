88e19768f8f9f88b2c3035372d5f098984c145e2
Aaron Pritzlaff <aaron@porpoiseltd.com>
2011-08-29 21:20:50 +0100

    removed sbt project dir as its not needed in sbt 0.10

--- src/main/scala/com/porpoise/ScalaBean.scala
+++ src/main/scala/com/porpoise/ScalaBean.scala
@@ -1,3 +1,11 @@
 package com.porpoise
 import java.util._
-case class ScalaBean(str : String, num : Int, date : Date = new Date()) 
+case class ScalaBean(str : String, num : Int, date : Date = new Date()) {
+	
+	def asJavaBean = new JavaBean(str)
+}
+object App {
+	def main(args : Array[String]) = {
+		println(ScalaBean("scala bean as java bean", 1).asJavaBean.getName)
+	}
+}
